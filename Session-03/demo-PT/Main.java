401d519dd413fb00c217d1655fb481d5b5e17880
Faris Abou-Saleh <superfaris@googlemail.com>
2022-01-28 13:32:33 +0000

    demo week 3 code from last part, about reduce

--- Session-03/demo-PT/Main.java
+++ Session-03/demo-PT/Main.java
@@ -43,6 +43,11 @@ public class Outline {
 
     public static void main(String... args) { // varargs alternative to String[]
 
+        // What does flatmap do?
+        // Stream.flatMap() helps in converting Stream<Collection<T>> to Stream<T>.
+        // for example:
+        // Before flattening :  [[1,2,3],[4,5],[6,7,8,9]]
+        // After flattering :   [1,2,3,4,5,6,7,8,9]
 
     /*
     List<String> pronouns = Arrays.asList("He", "She", "They");
@@ -75,13 +80,60 @@ public class Outline {
 
 
 
-        // What does flatmap do?
-        // Stream.flatMap() helps in converting Stream<Collection<T>> to Stream<T>.
-        // for example:
-        // Before flattening :  [[1,2,3],[4,5],[6,7,8,9]]
-        // After flattering :   [1,2,3,4,5,6,7,8,9]
+        // Reduction:
+
+        var s = Stream.of(1,2,3,4,5)
+                .reduce(0, (a, b) -> a + b);
+
+        // (0, 1) -> (0 + 1) = 1
+        // (1, 2) -> (1 + 2) = 3
+        // (3, 3) -> (3 + 3) = 6
+        // (6, 4) -> (6 + 4) = 10
+        // (10, 5) -> (10 + 5) = 15
+
+        var s2 = Stream.of(1,2,3,4,5)
+                .reduce(1, (a, b) -> a * b);
+
+        // (1, 1) -> (1 * 1) = 1
+        // (1, 2) -> (1 * 2) = 2
+        // (2, 3) -> (2 * 3) = 6
+        // (6, 4) -> (6 * 4) = 24
+        // (24, 5) -> (24 * 5) = 120
+
+        var s3 = Stream.of(2.0, 3.0, 4.0, 5.0)
+                .reduce(1.0, (a, b) -> a / b);
 
 
+        var s4 = Stream.of(2)
+                .reduce((a, b) -> a + b);
+
+        assert s4.isEmpty() == false && s4.get() == 2;
+
+        var s5 = Stream.of(2, 3, 4, 5)
+                .reduce((a, b) -> a + b);
+
+        assert s4.isEmpty() == false && s4.get() == 14;
+
+
+        var s6 = Stream.<Integer>empty()
+                .reduce((a, b) -> a + b);
+        assert s4.isEmpty() == true;
+
+        // [1, 2, 3, 4] --> "1, 2, 3, 4"
+
+        var s7 = Stream.of(1,2,3,4)
+                .reduce("hello",
+                        (string, i) -> string + "..." + i.toString() + "!",
+                        (string1, string2) -> string1 + string2);
+
+        System.out.println("Reduction gives: " + s7);
+
+        var s8 = Stream.of(1,2,3,4)
+                .reduce("",
+                        (string, i) -> string.isEmpty() ? i.toString() : string + ", " + i.toString(),
+                        (string1, string2) -> string1 + string2);
+
+        System.out.println("Second reduction gives: " + s8);
 
 
     }
